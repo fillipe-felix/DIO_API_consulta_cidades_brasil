cf111745b36aa4b3bebfce75d11ced1d9cdc3b50
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-10 19:36:45 -0300

    Iniciando API de consulta de busca de cidades.

---
+++ src/main/java/com/consulta/cidades/dio_api_consulta_cidades_brasil/DioApiConsultaCidadesBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.consulta.cidades.dio_api_consulta_cidades_brasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApiConsultaCidadesBrasilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioApiConsultaCidadesBrasilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/consulta/cidades/dio_api_consulta_cidades_brasil/DioApiConsultaCidadesBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulta.cidades.dio_api_consulta_cidades_brasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApiConsultaCidadesBrasilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
